67dbfc76c99c71becf7c99ab28287f2a1c23f348
Terry-Elena <40982317+Terry-Elena@users.noreply.github.com>
2018-07-09 11:19:08 +0300

    Change h2

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     <main role="main">
 
       <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <h2> For Git </h2>
         <ol class="carousel-indicators">
           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
           <li data-target="#myCarousel" data-slide-to="1"></li>
